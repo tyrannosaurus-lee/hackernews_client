7536199e5c02902ee96225ac7fc405a963af0120
tyrannosaurus-lee <miaow106@gmail.com>
2021-07-19 10:59:09 +0900

    CH03_03. 두 개의 화면을 가진 웹앱 - 중복 제거

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'http://api.hnpwa.com/v0/news/1.json';
@@ -41,7 +42,7 @@ for(let i=0; i<10; i++){
     ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
-document.getElementById('root').appendChild(content);
+container.appendChild(ul);
+container.appendChild(content);
 
 
--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -118,6 +118,7 @@ parcelRequire = (function (modules, cache, entry, globalName) {
 
   return newRequire;
 })({"app.js":[function(require,module,exports) {
+var container = document.getElementById('root');
 var ajax = new XMLHttpRequest();
 var content = document.createElement('div');
 var NEWS_URL = 'http://api.hnpwa.com/v0/news/1.json';
@@ -156,8 +157,8 @@ for (var i = 0; i < 10; i++) {
   ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
-document.getElementById('root').appendChild(content);
+container.appendChild(ul);
+container.appendChild(content);
 },{}],"../../../AppData/Roaming/npm/node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -186,7 +187,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57006" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60783" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
